ce843aac4e6369dd33b208abea0d47713bed2a3f
Keijiro Takahashi <keijiro@gmail.com>
2024-10-15 17:34:15 +0900

    Prompt generator

--- Dcam2Visualizer/Assets/0.Common/Scripts/PromptGenerator.cs
+++ Dcam2Visualizer/Assets/0.Common/Scripts/PromptGenerator.cs
@@ -4,25 +4,73 @@ namespace Dcam2 {
 
 public sealed class PromptGenerator : MonoBehaviour
 {
-    [SerializeField] string[] _basePrompts = null;
-    [SerializeField] ImageGenerator _generator = null;
+    #region Scene object reference
 
-    (bool shift, int index) _select;
+    [System.Serializable]
+    struct EffectSuffix
+    {
+        public GameObject Target;
+        public string Prompt;
+
+        public EffectSuffix(GameObject target, string prompt)
+          => (Target, Prompt) = (target, prompt);
+    }
+
+    [SerializeField] ImageGenerator _generator = null;
+    [SerializeField] string _prefix = null;
+    [SerializeField] string[] _bodies = null;
+    [SerializeField] EffectSuffix[] _suffixes = null;
+
+    #endregion
+
+    #region Public properties
+
+    public bool SelectorShift
+      { get; set; }
+
+    public int SelectorIndex
+      { get => _selectorIndex; set => SetSelectorIndex(value); }
+
+    #endregion
+
+    #region Property backing members
+
+    int _selectorIndex;
+
+    void SetSelectorIndex(int index)
+    {
+        _selectorIndex = index;
+        UpdatePrompt();
+    }
+
+    #endregion
+
+    #region Prompt generator
 
     void UpdatePrompt()
     {
-        var i = (_select.shift ? 6 : 0) + _select.index;
-        _generator.Prompt = _basePrompts[i % _basePrompts.Length];
+        var prompt = _prefix + " ";
+
+        var idx = (SelectorShift ? 6 : 0) + _selectorIndex;
+        prompt += _bodies[idx % _bodies.Length];
+
+        for (var i = 0; i < _suffixes.Length; i++)
+        {
+            if (_suffixes[i].Target.activeSelf)
+                prompt += " covered with " + _suffixes[i].Prompt;
+        }
+
+        _generator.Prompt = prompt;
     }
 
-    public void SetBaseIndexShift(bool shift)
-        => _select.shift = shift;
+    #endregion
 
-    public void SetBaseIndex(int index)
-    {
-        _select.index = index;
-        UpdatePrompt();
-    }
+    #region MonoBehaviour implementation
+
+    void Start()
+      => UpdatePrompt();
+
+    #endregion
 }
 
 } // namespace Dcam2
